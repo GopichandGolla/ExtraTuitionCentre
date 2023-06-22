2ed4a03d27905dc1bc0a5a1615246399fffa468e
Gopi Golla <zg22abe@herts.ac.uk>
2023-06-22 22:07:09 +0530

    Initial Commit - main function

---
+++ Main/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+// Enum to represent subject areas
+enum Subject {
+    ENGLISH_COMPREHENSION,
+    ENGLISH_WRITING,
+    MATH,
+    NUMERICAL_REASONING,
+    VERBAL_REASONING,
+    NON_VERBAL_REASONING
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TuitionSystem tuitionSystem = new TuitionSystem();
+
+    }
+
+    private static Date parseDate(String dateString) {
+
+        return null;
+    }
+}
+
